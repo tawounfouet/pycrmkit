6f5e837cb5a8f35292cbf847b6873b492de6b6d4
Thomas AWOUNFOUET <78394330+tawounfouet@users.noreply.github.com>
2026-09-26 10:55:57 +0200

    feat(sqlalchemy): expose adapter foundation

---
+++ src/pycrmkit/storage/sqlalchemy/__init__.py
@@ -0,0 +1,21 @@
+"""Optional SQLAlchemy persistence adapter foundation."""
+
+from pycrmkit.storage.sqlalchemy.base import (
+    Base,
+    NAMING_CONVENTION,
+    TimestampedModelMixin,
+    metadata,
+)
+from pycrmkit.storage.sqlalchemy.mapping import FunctionalMapper, SQLAlchemyMapper
+from pycrmkit.storage.sqlalchemy.models import *  # noqa: F403
+from pycrmkit.storage.sqlalchemy.models import __all__ as _models_all
+
+__all__ = [
+    "Base",
+    "FunctionalMapper",
+    "NAMING_CONVENTION",
+    "SQLAlchemyMapper",
+    "TimestampedModelMixin",
+    "metadata",
+    *_models_all,
+]
