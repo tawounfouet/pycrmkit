e64d619456ec7a8e717cd1b1d83b294bd794d06a
Thomas AWOUNFOUET <78394330+tawounfouet@users.noreply.github.com>
2026-09-26 12:58:52 +0200

    style(migrations): remove unused Session import

--- tests/migrations/test_alembic_migrations.py
+++ tests/migrations/test_alembic_migrations.py
@@ -11,7 +11,7 @@ from alembic.autogenerate import compare_metadata
 from alembic.config import Config
 from alembic.migration import MigrationContext
 from sqlalchemy import Engine, inspect, text
-from sqlalchemy.orm import Session, sessionmaker
+from sqlalchemy.orm import sessionmaker
 
 from pycrmkit.contacts import Contact, ContactId
 from pycrmkit.storage.sqlalchemy import Base, SQLAlchemyUnitOfWork
