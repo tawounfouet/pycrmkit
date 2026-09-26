65b83d344a8a6c36d960ea05adca8806d8045ad5
Thomas AWOUNFOUET <78394330+tawounfouet@users.noreply.github.com>
2026-09-26 11:55:00 +0200

    style(sqlalchemy): separate third-party and package imports

--- tests/adapters/sqlalchemy/test_opportunities_repository.py
+++ tests/adapters/sqlalchemy/test_opportunities_repository.py
@@ -4,6 +4,7 @@ from uuid import UUID
 
 import pytest
 from sqlalchemy.orm import Session
+
 from pycrmkit.contacts import ContactId
 from pycrmkit.opportunities import Opportunity, OpportunityId, OpportunityRepository
 from pycrmkit.organizations import OrganizationId
