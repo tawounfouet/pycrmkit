8dcf69d746f5bb3c22789021b3f3b5550c05828a
Thomas AWOUNFOUET <78394330+tawounfouet@users.noreply.github.com>
2026-09-26 11:52:25 +0200

    style(sqlalchemy): normalize contract test imports

--- tests/adapters/sqlalchemy/test_relationships_repository.py
+++ tests/adapters/sqlalchemy/test_relationships_repository.py
@@ -7,7 +7,6 @@ from uuid import UUID
 
 import pytest
 from sqlalchemy.orm import Session
-
 from pycrmkit.contacts import ContactId
 from pycrmkit.organizations import OrganizationId
 from pycrmkit.relationships import (
